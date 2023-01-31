d775289ea7cb1a763226060cad9b290649a4c4e2
ybh <1361005897@qq.com>
2023-01-31 14:22:47 +0800

    master commit

--- src/main/java/com/ybh/git/GitTest.java
+++ src/main/java/com/ybh/git/GitTest.java
@@ -10,5 +10,7 @@ public class GitTest {
         System.out.println("this is Git test.........");
         System.out.println("this is Git test1.........");
         System.out.println("this is Git test2.........");
+
+        System.out.println("master");
     }
 }
