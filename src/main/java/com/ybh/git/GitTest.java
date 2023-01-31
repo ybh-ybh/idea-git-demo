df5e4563b84fc7ec5522e6d21e46ae49fc9a14d4
ybh <1361005897@qq.com>
2023-01-31 13:35:47 +0800

    first commit

---
+++ src/main/java/com/ybh/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.ybh.git;
+
+/**
+ * @author ybh  Email:1361005897@qq.com
+ * @Description
+ * @create 2023-01-31-13:31
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("this is Git test.........");
+    }
+}
